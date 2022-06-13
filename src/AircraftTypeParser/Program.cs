f35f14c91191fc64128661a53c0de482e3da4c7c
Leo Chen <Leo@zhangbudademao.com>
2022-06-13 21:07:29 +0800

    This is the begining of the project. Converting code and refactoring from another private .NET Framework project.(PrfLauncher@0.1.3.9+)

---
+++ src/AircraftTypeParser/Program.cs
@@ -0,0 +1,67 @@
+﻿using DmCommons;
+using Newtonsoft.Json;
+using System.Net;
+using System.Text;
+
+namespace AircraftTypeParser
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        private static void Main(string[] args)
+        {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+
+            string str = Http.PostBody("https://www4.icao.int/doc8643/External/AircraftTypes");
+
+            if (!string.IsNullOrEmpty(str))
+            {
+                Aircraft[] aircrafts = JsonConvert.DeserializeObject<Aircraft[]>(str);
+
+                SortedDictionary<string, string> aircrafts_by_designator = new SortedDictionary<string, string>();
+
+                foreach (Aircraft a in aircrafts)
+                {
+                    if (aircrafts_by_designator.ContainsKey(a.Designator))
+                        aircrafts_by_designator[a.Designator] += $",{a.ModelFullName}";
+                    else
+                        aircrafts_by_designator.Add(a.Designator, $"{a.Designator}\t{a.WTC.Substring(a.WTC.Length - 1, 1)}{a.Description}\t{a.ManufacturerCode}\t{a.ModelFullName}");
+                }
+
+                StringBuilder sb = new StringBuilder();
+                foreach (KeyValuePair<string, string> i in aircrafts_by_designator)
+                {
+                    sb.AppendLine(i.Value);
+                }
+
+                File.WriteAllText("ICAO_Aircraft.txt", sb.ToString());
+                sb = null;
+            }
+        }
+
+        #endregion Private Methods
+
+        #region Private Classes
+
+        private class Aircraft
+        {
+            #region Public Fields
+
+            public string AircraftDescription;
+            public string Description;
+            public string Designator;
+            public string EngineCount;
+            public string EngineType;
+            public string ManufacturerCode;
+            public string ModelFullName;
+            public string WTC;
+            public string WTG;
+
+            #endregion Public Fields
+        }
+
+        #endregion Private Classes
+    }
+}
---
+++ src/ReleaseIndexGenerator/Program.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+
+namespace ReleaseIndexGenerator
+{
+    internal class Program
+    {
+        #region Private Methods
+
+        /// <summary> 获取文件 </summary> 
+        /// <param name="dir">查找目录</param> 
+        /// <param name="top">根目录</param>
+        /// <param name="files">引用，文件字典<相对路径,MD5></param>
+        private static void GetFiles(DirectoryInfo dir, string top, ref Dictionary<string, string> files)
+        {
+            if (dir.GetFiles().ToList().Exists((e) => { return e.Name == ".releaseignoreflag"; }))
+            {
+                return;
+            }
+
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                files.Add(file.FullName.Replace(top, ""), EncryptHelper.GetMD5FromFile_32(file.FullName).ToLower());
+            }
+
+            foreach (DirectoryInfo sub_dir in dir.GetDirectories())
+            {
+                GetFiles(sub_dir, top, ref files);
+            }
+        }
+
+        private static void Main(string[] args)
+        {
+            if (args.Length > 0 && !string.IsNullOrEmpty(args[0]) && Directory.Exists(args[0]))
+            {
+                Dictionary<string, string> files = new Dictionary<string, string>();
+
+                GetFiles(new DirectoryInfo(args[0]), args[0], ref files);
+
+                File.WriteAllText($"{args[0]}\\index.json", JsonConvert.SerializeObject(files));
+            }
+        }
+
+        #endregion Private Methods
+    }
+}
